b2c8263fff6b73a874e845ce1f7258b90bfeb6b9
seguridadindrustrial <carolinablanco8419@gmail.com>
2026-09-16 12:01:55 -0400

    Apuntar al nuevo despliegue del Apps Script con columnas de seguimiento y total de horas

--- js/data.js
+++ js/data.js
@@ -1091,4 +1091,4 @@ const EQUIPO_RUTINA = {
     "TOPE FRANCES A GAS 2": "Rutina Cocinas",
 };
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwpStam6K0NFlpAJn2MfumDhxGvOlq-7IagFGry2KuOr67VxobNYn1DpUccyQWYtWqA0w/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz9PzTBDF4pMA_wth7LxJifWz-BAifRPtjEFDBhwclSdtDP8GJ8gU_IFxI0SYlOrIbeaQ/exec";
